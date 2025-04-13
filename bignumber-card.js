58f81dc2cf13aaa4819a36e1dde538ce2f5b228c
sicarr1976 <si@sicarr.co.uk>
2025-04-13 17:59:39 +0100

    Update bignumber-card.js

--- bignumber-card.js
+++ bignumber-card.js
@@ -42,7 +42,7 @@ class BigNumberCard extends HTMLElement {
       }
       #value {
         font-size: calc(var(--base-unit) * 1.3);
-        line-height: calc(var(--base-unit) * 1.3);
+        line-height: calc(var(--base-unit) * 1.0);
         color: var(--bignumber-color);
       }
       #value small{opacity: ${cardConfig.opacity}}
